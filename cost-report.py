bad563da1166b2845667ad079ec9d5d2a6aaff92
RRahul19 <rahulr.cbe@gmail.com>
2021-01-20 18:53:20 +0530

    code modified

--- cost-report.py
+++ cost-report.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
 import json
 import locale
+import datetime 
 import subprocess
-from datetime import datetime
 from calendar import monthrange
 from dateutil import relativedelta
 
 class bcolors:
     CYAN = '\033[96m'
-    MAGENTA = '\033[95m'
-    GREY = '\033[90m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
     WARNING = '\033[93m'
-    FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 months = {"01":"January", "02":"February", "03":"March", "04":"April", "05":"May", "06":"June", "07":"July", "08":"August", "09":"September", "10":"October", "11":"November", "12":"December"}
-# aws ce get-cost-and-usage --time-period Start=2020-12-01,End=2020-12-31 --granularity MONTHLY --metrics "BlendedCost" "UnblendedCost" "UsageQuantity"
 
 yr = int; mon1 =int; date = int; mon = str;
 print("Press 1 for current month Bill or Press 2 for specific mon/yr: "); 
@@ -33,10 +25,16 @@ elif i == '2':
 else:
     print("Invalid Selection. Please enter either 1 or 2")
 
-date = monthrange(yr, mon1)[1]
+dates = monthrange(yr, mon1)[1]
 mon = "{:02d}".format(mon1)
 
-cmd = 'aws ce get-cost-and-usage --time-period Start={}-{}-01,End={}-{}-{} --granularity MONTHLY --metrics "BlendedCost"'.format(yr, mon, yr, mon, str(date))
+end = "{}-{}-{}".format(yr, mon, str(dates))
+end1 = datetime.datetime.strptime(end, '%Y-%m-%d')  #convert string to datetime
+end2 = end1 + datetime.timedelta(days = 1) 		#add 1 day 
+End_date = str(end2).split(" ",1)			#convert date to string then split date & time
+
+cmd = 'aws ce get-cost-and-usage --time-period Start={}-{}-01,End={} --granularity MONTHLY --metrics "BlendedCost"'.format(yr, mon, End_date[0])
+# aws ce get-cost-and-usage --time-period Start=2020-12-01,End=2020-12-31 --granularity MONTHLY --metrics "BlendedCost" "UnblendedCost" "UsageQuantity"
 
 try:
     output = subprocess.check_output(cmd.format(yr,mon, yr, mon), shell=True)
